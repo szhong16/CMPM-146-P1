c2cbe294222904a8e8f13645c54ab28eef0172b5
Jiaying330 <jiaying.hou330@gmail.com>
2021-10-02 21:15:44 +0800

    A* bidirection in progress

--- src/nm_pathfinder.py
+++ src/nm_pathfinder.py
@@ -32,6 +32,7 @@ def find_path (source_point, destination_point, mesh):
     print("dst:")
     print(dst)
 
+    # check if arguments exist
     if dst == None or src == None:
         print("path not found")
         return path, boxes
@@ -42,16 +43,17 @@ def find_path (source_point, destination_point, mesh):
     path.append(source_point)
 
     # for graph search algorithm
-    detail_points = {src : source_point}
-    detail_points[dst] = destination_point 
-    queue = [] # queue of boxes
-    came_from_forward = {src : None}
-    cost_so_far_forward = {src : 0}
-    
-    came_from_backward = {dst : None}
-    cost_so_far_backward = {dst : 0}
+    queue = [] # queue of boxes (priority, curr_box, goal_box)
     heappush(queue, (0, src, dst))
     heappush(queue, (0, dst, src))
+    # forward direction tables src -> dst
+    detail_points_forward = {src : source_point}
+    came_from_forward = {src : None}
+    cost_so_far_forward = {src : 0}
+    # backward direction tables src <- dst
+    detail_points_backward = {dst : destination_point}
+    came_from_backward = {dst : None}
+    cost_so_far_backward = {dst : 0}
  
     # A* searching algorithm
     while queue:
@@ -59,46 +61,54 @@ def find_path (source_point, destination_point, mesh):
         # print("current_box: ", current_box)
         # print("current_goal: ", current_goal)
         if current_box == current_goal or (came_from_forward.get(current_box) is not None and came_from_backward.get(current_box) is not None):
+            print("current_box: ", current_box)
+            print("current_goal: ", current_goal)
             if current_box == current_goal:
                 print("current_box == current_goal")
                 if current_goal == dst:
                     box_path = path_to_box(current_box, came_from_forward)
                     print("found destination: current_box == dst")
+                    path = box_path_to_detail_point_path(box_path, detail_points_forward)
                     break
                 elif current_goal == src:
                     box_path = path_to_box(current_box, came_from_backward)
                     print("found destination: current_box == src")
+                    path = box_path_to_detail_point_path(box_path, detail_points_backward)
+                    break
+
+            # if two directions intersects
             else:
                 box_path_1 = path_to_box(current_box, came_from_forward)
                 print("came_from_forward box_path_1: ", box_path_1)
                 box_path_2 = path_to_box(current_box, came_from_backward)
                 print("came_from_backward box_path_2: ", box_path_2)
                 box_path_2 = list(reversed(box_path_2))
-                box_path = box_path_1 + box_path_2
+                path = box_path_to_detail_point_path(box_path_1, detail_points_forward) + box_path_to_detail_point_path(box_path_2, detail_points_backward)
+                # box_path = box_path_1 + box_path_2
                 print("found destination: current_box != current_goal")
             break
-
+        
         for next in adj[current_box]:
             boxes[next] = current_box
             # chech which is the source point
             if current_goal == dst:
                 # print("current_goal == dst")
-                detail_point_and_cost = find_detail_points(current_box, next, detail_points, source_point)
+                detail_point_and_cost = find_detail_points(current_box, next, detail_points_forward, current_goal)
                 new_cost = current_cost + detail_point_and_cost[1]  
                 if next not in cost_so_far_forward or new_cost < cost_so_far_forward[next]:
-                    if next not in detail_points.keys():
-                        detail_points[next] = detail_point_and_cost[0]
+                    if next not in detail_points_forward.keys():
+                        detail_points_forward[next] = detail_point_and_cost[0]
                     cost_so_far_forward[next] = new_cost
                     priority = new_cost + euclidean_distance(current_goal, detail_point_and_cost[0])
                     heappush(queue, (priority, next, current_goal))
                     came_from_forward[next] = current_box
             elif current_goal == src:  
                 # print("current_goal == src")
-                detail_point_and_cost = find_detail_points(current_box, next, detail_points, destination_point)
+                detail_point_and_cost = find_detail_points(current_box, next, detail_points_backward, current_goal)
                 new_cost = current_cost + detail_point_and_cost[1]  
                 if next not in cost_so_far_backward or new_cost < cost_so_far_backward[next]:
-                    if next not in detail_points.keys():
-                        detail_points[next] = detail_point_and_cost[0]
+                    if next not in detail_points_backward.keys():
+                        detail_points_backward[next] = detail_point_and_cost[0]
                     cost_so_far_backward[next] = new_cost
                     priority = new_cost + euclidean_distance(current_goal, detail_point_and_cost[0])
                     heappush(queue, (priority, next, current_goal))
@@ -108,14 +118,21 @@ def find_path (source_point, destination_point, mesh):
     print("--------------------")
 
     # box_path = list(dict.fromkeys(box_path))
-    print("box_path = ", box_path)
-    for next in box_path:
-        print("in 'for next in box_path', box = ", next)
-        path.append(detail_points[next])
+    # print("box_path = ", box_path)
+    # for next in box_path:
+    #     print("in 'for next in box_path', box = ", next)
+    #     path.append(detail_points[next])
 
     print("path:")
     print(path)
-    return path, allBoxes
+    return path, boxes
+
+# given a box_path, find the exact landing points to a box from detail_points list
+def box_path_to_detail_point_path(box_path, detail_points):
+    path = []
+    for next in box_path:
+        path.append(detail_points[next])
+    return path
 
 # given a coordinates of a point, find the box that the point is located at
 def find_box(box_list, point):
@@ -128,7 +145,7 @@ def find_box(box_list, point):
 def path_to_box(box, paths):
     if box == None:
         return []
-    print("in path_to_box, box = ", box)
+    # print("in path_to_box, box = ", box)
     return path_to_box(paths[box], paths) + [box]
 
 # find the detail points to land on boxes. Returns (point(x,y), cost from box_1 to box_2)
